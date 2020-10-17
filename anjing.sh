d361a9ca6bccd2cd11f8afc2bd6da1560a33e7d2
WhoMHw <71681090+WhoMHw@users.noreply.github.com>
2020-10-17 13:46:47 +0700

    Add files via upload

---
+++ anjing.sh
@@ -0,0 +1,37 @@
+#!/xbin/bash
+#toolsWhoMHw#
+
+echo "////////////////////////////////////////////////////////////////////////////////////////"
+echo "                                  Author:WhoMHw                                           "
+echo "                                                                               //"
+echo "                                                                               \\"
+echo "//////////////////////////////////////////////////////////////////////////////////////"
+
+   echo "selamat datang kaka,siapa nama kaka?" #tulisan keluar
+ read nick #membaca tulisan masuk
+ clear
+ echo "##########"
+  echo "##########         ini toool milik bersama                                                       ######"
+
+   echo  "#########               WhoMHw and MR.WR.py                                                    ######"
+    echo "#########                                                                                      ######"
+     echo "##########                                                                                    ######"
+      echo "##########                                                                                   ######"
+       echo "###############№##################################################################################"
+        echo  "############################№#############################################################"
+         echo "######################################№№#################################################"
+echo "selamat datang di tools ini" $nick
+echo "tunggg bentarrr................."
+echo "masukkan target kamu : "
+read target #target
+echo "input file kamu :"
+read file #file
+echo "tunggu bentar ya tod w lagi apluod scrpit lu...."
+curl -T /storage/emulated/0/$file $target
+echo
+echo "==================================="
+echo " Tools ini gratis"
+echo " Who MHw and MR.WR.py"
+echo " Dilarang menjual belikan tools ini"
+echo " Dilarang menyalahgunakan tools ini :)"
+echo "==================================="
